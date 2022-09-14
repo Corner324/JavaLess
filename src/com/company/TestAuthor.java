2fdf77ab080957097d5f4602cdbc66ffb10e9e81
Corner324 <mosenn@mail.ru>
2022-09-14 17:56:36 +0300

    Connect

--- src/com/company/TestAuthor.java
+++ src/com/company/TestAuthor.java
@@ -3,7 +3,7 @@ package com.company;
 class TestAuthor{
     public static void main(String[] args) {
 
-        Author auth = new Author("Name","Email",'M');
+        Author auth = new Author("Name","Email",'F');
 
         System.out.println(auth);
 
