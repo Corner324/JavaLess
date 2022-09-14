e23bf970a34457dd413401faa3f832ac0faaa7d5
Corner324 <mosenn@mail.ru>
2022-09-14 17:46:33 +0300

    Merge

---
+++ src/com/company/Author.java
@@ -0,0 +1,34 @@
+package com.company;
+
+public class Author {
+
+    String name = "Mark";
+    String email;
+    char gender;
+
+    Author(String name, String email, char gender){
+        this.name = name;
+        this.email = email;
+        this.gender = gender;
+    }
+
+    String getName(){
+        return this.name;
+    }
+
+    String getEmail(){
+        return this.email;
+    }
+
+    char getGender(){
+        return this.gender;
+    }
+
+    void setEmail(String email){
+        this.email = email;
+    }
+
+    public String toString(){
+        return "Name is " + name + " email is " + email + " and gender is " + gender;
+    }
+}
---
+++ src/com/company/TestAuthor.java
@@ -0,0 +1,11 @@
+package com.company;
+
+class TestAuthor{
+    public static void main(String[] args) {
+
+        Author auth = new Author("Name","Email",'M');
+
+        System.out.println(auth);
+
+    }
+}
