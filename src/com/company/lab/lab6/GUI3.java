64feba768b6289d1d760b901a390eee5144633f6
Corner324 <mosenn@mail.ru>
2022-11-01 23:40:01 +0300

    6 lab

--- src/com/company/lab/lab6/GUI3.java
+++ src/com/company/lab/lab6/GUI3.java
@@ -5,17 +5,25 @@ import java.awt.event.*;
 
 public class GUI3 extends JFrame
 {
-    static int attempts = 0;
+    int MAX = 20;
+    int MIN = 0;
+    static int attempts = 1;
+    static int number = 0;
     private JButton button = new JButton("Угадай");
-    private final JTextField input = new JTextField("Какой-то текст",10);
+    private final JTextField input = new JTextField("",10);
+    private final JLabel label = new JLabel("Попытка 1  ");
 
 
     public GUI3()
     {
-        super("Лабораторная 6 | Игра-угадайка");
+        super("Игра-угадайка");
 
         button.setPreferredSize(new Dimension(200, 30));
         input.setPreferredSize(new Dimension(200, 30));
+        label.setFont(new Font("Courier", Font.BOLD,14));
+        input.setFont(new Font("Courier", Font.BOLD,12));
+
+        number = (int)(Math.random()*((MAX-MIN)+1))+MIN;
 
         // Панель содержимого
         Container container = getContentPane();
@@ -25,15 +33,20 @@ public class GUI3 extends JFrame
          */
         container.setLayout (new FlowLayout(FlowLayout.CENTER));
         // добавляем компоненты
+        container.add(label);
         container.add(input);
         container.add(button);
 
 
+
         button.addActionListener(new ButtonEventListener1());
 
-
-         setSize(280,130);
-         setResizable(false);
+        Dimension dimension = Toolkit.getDefaultToolkit().getScreenSize();
+        int x = (int) (((dimension.getWidth() - this.getWidth()) / 2) - 280);
+        int y = (int) (((dimension.getHeight() - this.getHeight()) / 2) - 130);
+        this.setLocation(x, y);
+        setSize(280,130);
+        setResizable(false);
 
 
     }
@@ -41,19 +54,31 @@ public class GUI3 extends JFrame
 
     class ButtonEventListener1 implements ActionListener{
         public void actionPerformed(ActionEvent e){
-            if (attempts > 3) {
-                String messege = "Количество попыток закончилось \n Вы проиграли. ";
-                JOptionPane.showMessageDialog(null,messege, "output", JOptionPane.PLAIN_MESSAGE);
-                attempts = 0;
+            if (input.getText().isEmpty() ){
+                JOptionPane.showMessageDialog(null,"Впишите в поле выше число между 0 и 20", "Ошибка", JOptionPane.PLAIN_MESSAGE);
             }
-            /*
-            label1.setText(" Result: " + scoreMilan + " X " + scoreMadrid);
-            label2.setText(" Last Scorer: AC Milan" );
-            if (scoreMilan > scoreMadrid){
-                label3.setText(" Winner: AC Milan " );
-            }
+            else {
+                if (attempts == 3) {
+                    if (Integer.parseInt(input.getText()) != number) {
+                        String messege = "Вы не угадали. Загадное число - " + number + "\nКоличество попыток закончилось. \n                     Игра проиграна. ";
+                        JOptionPane.showMessageDialog(null, messege, "ПРОИГРЫШ", JOptionPane.PLAIN_MESSAGE);
+                        input.setText(" ");
+                        number = (int) (Math.random() * ((MAX - MIN) + 1)) + MIN;
+                        attempts = 0;
+                    }
 
-             */
+                }
+                if ((input.getText().equals("")) | (Integer.parseInt(input.getText()) == number) ) {
+                    String messege2 = "Вы верно угадали число! ";
+                    number = (int) (Math.random() * ((MAX - MIN) + 1)) + MIN;
+                    JOptionPane.showMessageDialog(null, messege2, "ПОБЕДА", JOptionPane.PLAIN_MESSAGE);
+                    attempts = 1;
+                } else {
+                    attempts++;
+                    label.setText("Попытка " + attempts + "  ");
+
+                }
+            }
         }
     }
 
